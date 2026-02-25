f69be6711ee09a16353ca13ae9e6a2bbec803be8
GG <giani.goras@gmail.com>
2026-02-25 14:35:04 +0200

    Add localhost:4200 to CORS configuration for development

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
         : [
             'http://localhost:3000',
             'http://localhost:3001',
+            'http://localhost:4200',
             'http://localhost:5173',
           ],
     credentials: true,
